f211e59795048073b23db01ec32a5ac2cae5e45e
Appu Goundan <appu@google.com>
2017-05-02 10:19:17 -0400

    Add open api doc generation task

---
+++ src/main/java/com/google/cloud/tools/gradle/endpoints/framework/server/task/GenerateOpenApiDocsTask.java
@@ -0,0 +1,129 @@
+/*
+ *  Copyright (c) 2017 Google Inc. All Right Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.google.cloud.tools.gradle.endpoints.framework.server.task;
+
+import com.google.api.server.spi.tools.EndpointsTool;
+import com.google.api.server.spi.tools.GetOpenApiDocAction;
+import com.google.common.base.Strings;
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import org.gradle.api.DefaultTask;
+import org.gradle.api.plugins.JavaPluginConvention;
+import org.gradle.api.tasks.Input;
+import org.gradle.api.tasks.InputDirectory;
+import org.gradle.api.tasks.Optional;
+import org.gradle.api.tasks.OutputDirectory;
+import org.gradle.api.tasks.SourceSet;
+import org.gradle.api.tasks.TaskAction;
+
+/** Endpoints task to download a openapi document from the endpoints service. */
+public class GenerateOpenApiDocsTask extends DefaultTask {
+
+  // classesDir is only for detecting that the project has changed
+  private File classesDir;
+
+  private File openApiDocDir;
+  private File webAppDir;
+  private List<String> serviceClasses;
+  private String hostname;
+
+  @InputDirectory
+  public File getClassesDir() {
+    return classesDir;
+  }
+
+  public void setClassesDir(File classesDir) {
+    this.classesDir = classesDir;
+  }
+
+  @OutputDirectory
+  public File getOpenApiDocDir() {
+    return openApiDocDir;
+  }
+
+  public void setOpenApiDocDir(File openApiDocDir) {
+    this.openApiDocDir = openApiDocDir;
+  }
+
+  @InputDirectory
+  public File getWebAppDir() {
+    return webAppDir;
+  }
+
+  public void setWebAppDir(File webAppDir) {
+    this.webAppDir = webAppDir;
+  }
+
+  @Input
+  public List<String> getServiceClasses() {
+    return serviceClasses;
+  }
+
+  public void setServiceClasses(List<String> serviceClasses) {
+    this.serviceClasses = serviceClasses;
+  }
+
+  @Optional
+  @Input
+  public String getHostname() {
+    return hostname;
+  }
+
+  public void setHostname(String hostname) {
+    this.hostname = hostname;
+  }
+
+  /** Task entry point. */
+  @TaskAction
+  void generateOpenApiDocs() throws Exception {
+    getProject().delete(openApiDocDir);
+    openApiDocDir.mkdirs();
+
+    String classpath =
+        (getProject()
+                .getConvention()
+                .getPlugin(JavaPluginConvention.class)
+                .getSourceSets()
+                .getByName(SourceSet.MAIN_SOURCE_SET_NAME)
+                .getRuntimeClasspath())
+            .getAsPath();
+
+    List<String> params =
+        new ArrayList<>(
+            Arrays.asList(
+                GetOpenApiDocAction.NAME,
+                "-o",
+                computeOpenApiDocPath(),
+                "-cp",
+                classpath,
+                "-w",
+                webAppDir.getPath()));
+    if (!Strings.isNullOrEmpty(hostname)) {
+      params.add("-h");
+      params.add(hostname);
+    }
+    params.addAll(getServiceClasses());
+
+    new EndpointsTool().execute(params.toArray(new String[params.size()]));
+  }
+
+  private String computeOpenApiDocPath() {
+    return new File(openApiDocDir, "openapi.json").getPath();
+  }
+}
--- src/test/java/com/google/cloud/tools/gradle/endpoints/framework/ProjectTests.java
+++ src/test/java/com/google/cloud/tools/gradle/endpoints/framework/ProjectTests.java
@@ -35,8 +35,8 @@ import org.junit.Test;
 import org.junit.rules.TemporaryFolder;
 
 public class ProjectTests {
-
-  private static final String DEFAULT_URL = "https://myapi.appspot.com/_ah/api";
+  private static final String DEFAULT_HOSTNAME = "myapi.appspot.com";
+  private static final String DEFAULT_URL = "https://" + DEFAULT_HOSTNAME + "/_ah/api";
   private static final String DEFAULT_URL_PREFIX = "public static final String DEFAULT_ROOT_URL = ";
   private static final String DEFAULT_URL_VARIABLE =
       DEFAULT_URL_PREFIX + "\"https://myapi.appspot.com/_ah/api/\";";
@@ -45,6 +45,7 @@ public class ProjectTests {
       "build/endpointsDiscoveryDocs/testApi-v1-rest.discovery";
   private static final String API_JAVA_FILE_PATH =
       "testApi/src/main/java/com/example/testApi/TestApi.java";
+  private static final String OPEN_API_DOC_PATH = "build/endpointsOpenApiDocs/openapi.json";
 
   @Rule public final TemporaryFolder testProjectDir = new TemporaryFolder();
 
@@ -83,7 +84,7 @@ public class ProjectTests {
         GradleRunner.create()
             .withProjectDir(testProjectDir.getRoot())
             .withPluginClasspath()
-            .withArguments("endpointsClientLibs", "endpointsDiscoveryDocs")
+            .withArguments("endpointsClientLibs", "endpointsDiscoveryDocs", "endpointsOpenApi")
             .build();
 
     File discoveryDoc = new File(testProjectDir.getRoot(), DISC_DOC_PATH);
@@ -97,6 +98,12 @@ public class ProjectTests {
     Assert.assertEquals(1, clientLib.getParentFile().listFiles().length);
     String apiJavaFile = getFileContentsInZip(clientLib, API_JAVA_FILE_PATH);
     Assert.assertThat(apiJavaFile, CoreMatchers.containsString(DEFAULT_URL_VARIABLE));
+
+    File openApiDoc = new File(testProjectDir.getRoot(), OPEN_API_DOC_PATH);
+    Assert.assertTrue(openApiDoc.exists());
+    Assert.assertEquals(1, openApiDoc.getParentFile().listFiles().length);
+    String openApi = Files.toString(openApiDoc, Charsets.UTF_8);
+    Assert.assertThat(openApi, CoreMatchers.containsString(DEFAULT_HOSTNAME));
   }
 
   @Test
@@ -109,7 +116,7 @@ public class ProjectTests {
         GradleRunner.create()
             .withProjectDir(testProjectDir.getRoot())
             .withPluginClasspath()
-            .withArguments("endpointsClientLibs", "endpointsDiscoveryDocs")
+            .withArguments("endpointsClientLibs", "endpointsDiscoveryDocs", "endpointsOpenApiDocs")
             .build();
 
     File discoveryDoc = new File(testProjectDir.getRoot(), DISC_DOC_PATH);
@@ -124,6 +131,11 @@ public class ProjectTests {
     Assert.assertThat(
         apiJavaFile,
         CoreMatchers.containsString(DEFAULT_URL_PREFIX + "\"https://my.hostname.com/_ah/api/\";"));
+
+    File openApiDoc = new File(testProjectDir.getRoot(), OPEN_API_DOC_PATH);
+    String openApi = Files.toString(openApiDoc, Charsets.UTF_8);
+    Assert.assertThat(openApi, CoreMatchers.not(CoreMatchers.containsString(DEFAULT_HOSTNAME)));
+    Assert.assertThat(openApi, CoreMatchers.containsString("my.hostname.com"));
   }
 
   // inject a endpoints plugin configuration into the pom.xml
