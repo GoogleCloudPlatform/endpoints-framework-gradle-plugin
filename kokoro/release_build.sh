918d35511c96948b053034efde7274051bd2cefc
Qingyang Chen <qingyangc@google.com>
2017-10-30 13:27:48 -0400

    Adds Kokoro build scripts.

---
+++ kokoro/release_build.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Fail on any error.
+set -e
+# Display commands to stderr.
+set -x
+
+sudo /opt/google-cloud-sdk/bin/gcloud components update
+sudo /opt/google-cloud-sdk/bin/gcloud components install app-engine-java
+
+cd github/app-gradle-plugin
+./gradlew check prepareRelease
