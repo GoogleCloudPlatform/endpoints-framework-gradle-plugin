75f8a18ef407c569611c2f5e0ad61fd5c8051d22
Qingyang Chen <qingyangc@google.com>
2017-10-30 15:00:37 -0400

    Fixes release-build.sh.

--- kokoro/release_build.sh
+++ kokoro/release_build.sh
@@ -5,8 +5,5 @@ set -e
 # Display commands to stderr.
 set -x
 
-# sudo /opt/google-cloud-sdk/bin/gcloud components update
-# sudo /opt/google-cloud-sdk/bin/gcloud components install app-engine-java
-
 cd github/endpoints-framework-gradle-plugin
-./gradlew check
+./gradlew check prepareRelease
