eb9c54af653a50ad960b82623f6e077eb123d830
Qingyang Chen <qingyangc@google.com>
2017-10-31 11:22:03 -0400

    Remove code coverage comment.

--- kokoro/continuous.bat
+++ kokoro/continuous.bat
@@ -5,6 +5,5 @@ cd github/endpoints-framework-gradle-plugin
 rem skip format check, because it fails for some line ending weirdness
 rem and it's anyway checked on ubuntu
 call gradlew.bat check -x verifyGoogleJavaFormat
-REM curl -s https://codecov.io/bash | bash
 
 exit /b %ERRORLEVEL%
